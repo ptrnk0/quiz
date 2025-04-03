b2e3cdc61b226e93cf144e38640587a04fe0072b
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-03 23:24:33 +0300

    add questions slice for managing quiz questions state in Redux

---
+++ src/redux/questions/questionsSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { fetchQuestionsId } from "./questionsOps";
+import { IQuestionsState } from "./questions.types";
+
+const initialState: IQuestionsState = {
+  items: [],
+  loading: false,
+  error: null,
+};
+
+const questionsSlice = createSlice({
+  name: "questions",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchQuestionsId.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload.fields.quizList;
+      })
+      .addMatcher(isAnyOf(fetchQuestionsId.pending), (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addMatcher(isAnyOf(fetchQuestionsId.rejected), (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default questionsSlice.reducer;
