ff240b40442c92a6c1c7360d0bb0059aa0bf26cb
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-03 18:51:46 +0300

    refactor App component to include routing and layout; update CSS for font and styling enhancements

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,21 @@
-import "./app.css";
+import "./App.css";
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../../pages/Home/Home";
+import Quiz from "../../pages/Quiz/Quiz";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Suspense>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/:quizId" element={<Quiz />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
