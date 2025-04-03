2d4da68c4305cb7471532cd861ad40b775f01084
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-03 23:24:43 +0300

    add TypeScript interfaces for questions state and API response

---
+++ src/redux/questions/questions.types.ts
@@ -0,0 +1,15 @@
+interface IQuestionsItems {
+  sys: { id: string };
+}
+
+export interface IQuestionsIdResponse {
+  fields: {
+    quizList: IQuestionsItems[];
+  };
+}
+
+export interface IQuestionsState {
+  items: IQuestionsItems[];
+  loading: boolean;
+  error: null | unknown;
+}
