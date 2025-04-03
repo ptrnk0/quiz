ab11b57d26828724d168e68b4c7bf4b907952c9d
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-03 18:52:25 +0300

    add Header component with navigation link

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="sticky m-auto w-11/12">
+      <Link
+        to="/"
+        className="inline-block cursor-pointer py-1 text-4xl font-bold transition-colors duration-300 ease-in-out hover:text-pink-100"
+      >
+        <span className="stroke-text uppercase">q</span>uizzz
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
