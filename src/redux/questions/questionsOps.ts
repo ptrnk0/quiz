0353779074bf8182859e2b0cd0f069db4a5d06ee
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-03 23:24:39 +0300

    add async thunk for fetching question IDs from Contentful API

---
+++ src/redux/questions/questionsOps.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { IQuestionsIdResponse } from "./questions.types";
+
+const SPACE = import.meta.env.VITE_SPACE_ID;
+const ENVIRONMENTS = import.meta.env.VITE_ENVIRONMENTS;
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+
+axios.defaults.baseURL = "https://cdn.contentful.com";
+
+export const fetchQuestionsId = createAsyncThunk<IQuestionsIdResponse>(
+  "questions/fetchQuestions",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios.get(
+        `/spaces/${SPACE}/environments/${ENVIRONMENTS}/entries/5VLkNsPSoRFhqEFNPQpJKc?&access_token=${ACCESS_TOKEN}`,
+      );
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error instanceof Error ? error.message : "An error occurred",
+      );
+    }
+  },
+);
