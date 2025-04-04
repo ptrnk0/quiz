b2014ea3d934b318f4697e3dcc8255bc7ec4c08d
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-04 15:56:41 +0300

    simplify Layout component by removing unnecessary wrapper around Outlet

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,9 +5,7 @@ const Layout = () => {
   return (
     <div className="font-roboto flex min-h-screen flex-col bg-yellow-400 pb-10">
       <Header />
-      <main className="m-auto flex h-full w-11/12 grow-1 flex-col">
-        <Outlet />
-      </main>
+      <Outlet />
     </div>
   );
 };
