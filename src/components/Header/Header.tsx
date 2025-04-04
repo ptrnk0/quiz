e1802e2c3cefa533d776b7c18cea9c1b04b2b331
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-04 15:56:33 +0300

    update Header component to adjust font size for larger screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="sticky m-auto w-11/12">
       <Link
         to="/"
-        className="inline-block cursor-pointer py-1 text-4xl font-bold transition-colors duration-300 ease-in-out hover:text-pink-100"
+        className="inline-block cursor-pointer py-1 text-4xl font-bold transition-colors duration-300 ease-in-out hover:text-pink-100 md:text-6xl"
       >
         <span className="stroke-text uppercase">q</span>uizzz
       </Link>
