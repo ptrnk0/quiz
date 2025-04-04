c0e5306b86d0a3fb5e52f4580cf3bb442ad8d99c
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-04 15:56:55 +0300

    add Quiz component to display quiz details using QuizCard and enhance layout

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,5 +1,17 @@
+import { useParams } from "react-router-dom";
+import QuizCard from "../../components/QuizCard/QuizCard";
+
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const { quizId } = useParams();
+
+  return (
+    <main className="relative m-auto flex h-full w-11/12 grow-1 flex-col">
+      <QuizCard quizId={quizId} />
+      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 rotate-1 transform border-2 border-black bg-pink-100 lg:w-200"></div>
+      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 rotate-2 transform border-2 border-black bg-pink-100 lg:w-200"></div>
+      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 -rotate-2 transform border-2 border-black bg-pink-100 lg:w-200"></div>
+    </main>
+  );
 };
 
 export default Quiz;
