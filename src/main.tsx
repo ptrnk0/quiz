700ad236b0b3c99d18ffbec8f8d7d21a0b3c39eb
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-05 21:58:06 +0300

    move global styles to index.css and import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/App/App.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
+import App from "./components/App/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
