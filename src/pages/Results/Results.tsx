7f6101057853bf5ee0b638c3d6d28c44ca0bb72c
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-05 23:27:07 +0300

    update Results component styles for improved responsiveness

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -4,9 +4,9 @@ const Results = () => {
   return (
     <main className="relative m-auto flex h-full w-11/12 grow-1 flex-col">
       <ResultsInfo />
-      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 rotate-1 transform border-2 border-black bg-pink-100 lg:w-200"></div>
-      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 rotate-2 transform border-2 border-black bg-pink-100 lg:w-200"></div>
-      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 -rotate-2 transform border-2 border-black bg-pink-100 lg:w-200"></div>
+      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 rotate-1 transform border-2 border-black bg-pink-100 lg:h-1/2 lg:w-200"></div>
+      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 rotate-2 transform border-2 border-black bg-pink-100 lg:h-1/2 lg:w-200"></div>
+      <div className="absolute top-1/2 left-1/2 h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 -rotate-2 transform border-2 border-black bg-pink-100 lg:h-1/2 lg:w-200"></div>
     </main>
   );
 };
