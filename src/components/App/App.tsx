1d8c92c7c35887c9c3f6419ed16b34bdc6ef3ddb
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-05 22:03:04 +0300

    add App.css for text stroke styling and update App.tsx to use it

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import "./App.module.css";
+import "./App.css";
 import { Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { fetchQuestionsId } from "../../redux/questions/questionsOps";
