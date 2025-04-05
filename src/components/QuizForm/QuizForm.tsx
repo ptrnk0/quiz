f720b7273f904becab4c9d163209215a0500d6af
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-05 18:14:32 +0300

    refactor QuizForm component to handle multiple answer selections and improve score calculation logic

--- src/components/QuizForm/QuizForm.tsx
+++ src/components/QuizForm/QuizForm.tsx
@@ -1,23 +1,91 @@
-import { FormEvent } from "react";
-import { IQuizResponse } from "../../types/index.types";
+import { FormEvent, useEffect, useState } from "react";
+import { IQuizPossibleAnswers, IQuizResponse } from "../../types/index.types";
+import { useAppDispatch } from "../../hooks";
+import { addAnswers, changeScore } from "../../redux/answersSlice";
 
 interface IQuizFromProps {
   quiz: IQuizResponse;
+  answers: IQuizPossibleAnswers[] | undefined;
 }
 
-const QuizForm = ({ quiz }: IQuizFromProps) => {
+const QuizForm = ({ quiz, answers }: IQuizFromProps) => {
+  const dispatch = useAppDispatch();
+  const [selectedCorrect, setSelectedCorrect] =
+    useState<IQuizPossibleAnswers[]>();
+  const [selectedWrong, setSelectedWrong] = useState<IQuizPossibleAnswers[]>();
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    if (answers) {
+      setChecked(true);
+    }
+  }, [answers]);
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
     const formData = new FormData(form);
-    const selectedAnswer = formData.getAll("quiz");
+    const selectedIds = formData.getAll("quiz");
 
-    if (!selectedAnswer) {
-      alert("Пожалуйста, выберите вариант ответа!");
+    if (selectedIds.length === 0) {
+      alert("Please select one or more answers!");
       return;
     }
 
-    console.log("Выбранный ответ:", selectedAnswer);
+    const parsedSelectedIds = selectedIds.map((id) => parseInt(id.toString()));
+    const possibleAnswersForCurrentQuiz = quiz.possibleAnswers;
+
+    const selectedAnswers = possibleAnswersForCurrentQuiz.filter((answer) =>
+      parsedSelectedIds.includes(answer.id),
+    );
+
+    const score = checkAnswers(selectedAnswers);
+
+    dispatch(
+      addAnswers({
+        quizId: quiz.id,
+        selectedAnswers: selectedAnswers,
+      }),
+    );
+
+    dispatch(changeScore(score));
+  };
+
+  const checkAnswers = (selectedAnswers: IQuizPossibleAnswers[]): number => {
+    const correctAnswers = quiz.possibleAnswers.filter(
+      (answer) => answer.isCorrect,
+    );
+    const wrongAnswers = quiz.possibleAnswers.filter(
+      (answer) => !answer.isCorrect,
+    );
+
+    const selectedCorrectAnswers = selectedAnswers.filter((answer) =>
+      correctAnswers.some((correct) => correct.id === answer.id),
+    );
+    const selectedWrongAnswers = selectedAnswers.filter((answer) =>
+      wrongAnswers.some((wrong) => wrong.id === answer.id),
+    );
+
+    setSelectedCorrect(selectedCorrectAnswers);
+    setSelectedWrong(selectedWrongAnswers);
+
+    const totalCorrect = correctAnswers.length;
+    const correctCount = selectedCorrectAnswers.length;
+    const wrongCount = selectedWrongAnswers.length;
+
+    const rawScore = correctCount - wrongCount * 0.5;
+    const finalScore = Math.max(0, rawScore / totalCorrect);
+
+    return finalScore;
+  };
+
+  const labelColor = (id: number) => {
+    if (selectedCorrect?.some((answer) => answer.id === id)) {
+      return { backgroundColor: "green" };
+    } else if (selectedWrong?.some((answer) => answer.id === id)) {
+      return { backgroundColor: "red" };
+    }
+    return {};
   };
 
   return (
@@ -29,15 +97,17 @@ const QuizForm = ({ quiz }: IQuizFromProps) => {
       {quiz.possibleAnswers.map(({ id, text }) => (
         <div key={id}>
           <input
-            type="radio"
+            type={quiz.questionType === "multiple" ? "radio" : "checkbox"}
             id={`option-${id}`}
             name="quiz"
             className="peer hidden"
-            value={id}
+            defaultValue={id}
+            disabled={checked}
           />
           <label
             htmlFor={`option-${id}`}
             className="text-md flex h-full cursor-pointer items-center justify-center border-2 border-black bg-yellow-400 px-3 py-1 text-center transition-colors duration-300 ease-in peer-checked:bg-yellow-600 hover:bg-yellow-500 md:text-xl"
+            style={labelColor(id)}
           >
             {text}
           </label>
