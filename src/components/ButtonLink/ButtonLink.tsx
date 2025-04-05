5a0404fc9d16811c8cb6d3c827f4e353af3ce35a
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-05 19:03:48 +0300

    refactor ButtonLink component to simplify link handling and improve accessibility

--- src/components/ButtonLink/ButtonLink.tsx
+++ src/components/ButtonLink/ButtonLink.tsx
@@ -1,26 +1,15 @@
 import { Link } from "react-router-dom";
-import { IQuestionsItems } from "../../redux/questions/questions.types";
 
 interface IButtonLinkProps {
   children: string;
-  link: null | IQuestionsItems;
+  link: string | null | undefined;
 }
 
 const ButtonLink = ({ children, link }: IButtonLinkProps) => {
-  if (!link)
-    return (
-      <Link
-        to="/"
-        className="text-md pointer-events-none inline-block cursor-not-allowed border-2 border-black bg-yellow-400 px-3 py-1 text-gray-400 transition-colors duration-300 ease-in hover:bg-yellow-500 md:text-xl"
-      >
-        {children}
-      </Link>
-    );
-
   return (
     <Link
-      to={`/${link?.sys.id}`}
-      className="text-md inline-block cursor-pointer border-2 border-black bg-yellow-400 px-3 py-1 transition-colors duration-300 ease-in hover:bg-yellow-500 md:text-xl"
+      to={link ? `/${link}` : "/"}
+      className={`${link ?? "pointer-events-none cursor-not-allowed text-pink-100"} text-md inline-block cursor-pointer border-2 border-black bg-yellow-400 px-3 py-1 transition-colors duration-300 ease-in hover:bg-yellow-500 md:text-xl`}
     >
       {children}
     </Link>
