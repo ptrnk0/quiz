8377ca12ef89bad5823d62ed4e2d4a9ea6b3de0c
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-05 15:55:54 +0300

    enhance QuizCard component to manage quiz answers and improve quiz navigation

--- src/components/QuizCard/QuizCard.tsx
+++ src/components/QuizCard/QuizCard.tsx
@@ -1,22 +1,27 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../../hooks";
-import { IQuestionsItems } from "../../redux/questions/questions.types";
-import ButtonLink from "../ButtonLink/ButtonLink";
+import {
+  IAnswer,
+  IQuestionsItems,
+  IQuizResponse,
+} from "../../types/index.types";
 import QuizForm from "../QuizForm/QuizForm";
-import { IQuizResponse } from "../../types/index.types";
+import ButtonLink from "../ButtonLink/ButtonLink";
 
 interface IQuizCardProps {
   quizId: string | undefined;
 }
 
-axios.defaults.baseURL = "https://cdn.contentful.com";
-
 const QuizCard: React.FC<IQuizCardProps> = ({ quizId }) => {
   const [quiz, setQuiz] = useState<IQuizResponse>();
   const [prevQuiz, setPrevQuiz] = useState<null | IQuestionsItems>(null);
   const [nextQuiz, setNextQuiz] = useState<null | IQuestionsItems>(null);
-  const quizList = useAppSelector((state) => state.questions.items);
+  const [answersForCurrentQuiz, setAnswersForCurrentQuiz] = useState<
+    null | IAnswer[]
+  >(null);
+  const quizzesList = useAppSelector((state) => state.questions.items);
+  const allAnswers = useAppSelector((state) => state.answers.items);
 
   useEffect(() => {
     (async () => {
@@ -32,19 +37,28 @@ const QuizCard: React.FC<IQuizCardProps> = ({ quizId }) => {
   }, [quizId]);
 
   useEffect(() => {
-    const foundQuiz = quizList.find((quiz) => quiz.sys.id === quizId);
-    const currentQuizIndex = foundQuiz ? quizList.indexOf(foundQuiz) : -1;
+    if (allAnswers.length !== 0 && quiz) {
+      const answersForCurrentQuiz: IAnswer[] = allAnswers.filter((answer) => {
+        return answer.quizId === quiz.id;
+      });
+      setAnswersForCurrentQuiz(answersForCurrentQuiz);
+    }
+  }, [allAnswers, quiz]);
+
+  useEffect(() => {
+    const foundQuiz = quizzesList.find((quiz) => quiz.sys.id === quizId);
+    const currentQuizIndex = foundQuiz ? quizzesList.indexOf(foundQuiz) : -1;
 
     setPrevQuiz(null);
     setNextQuiz(null);
 
     if (currentQuizIndex > 0) {
-      setPrevQuiz(quizList[currentQuizIndex - 1]);
+      setPrevQuiz(quizzesList[currentQuizIndex - 1]);
     }
-    if (currentQuizIndex < quizList.length - 1 && currentQuizIndex >= 0) {
-      setNextQuiz(quizList[currentQuizIndex + 1]);
+    if (currentQuizIndex < quizzesList.length - 1 && currentQuizIndex >= 0) {
+      setNextQuiz(quizzesList[currentQuizIndex + 1]);
     }
-  }, [quizList, quizId]);
+  }, [quizzesList, quizId]);
 
   return (
     <section className="absolute top-1/2 left-1/2 z-1 m-auto flex h-3/4 w-10/12 grow-1 -translate-x-1/2 -translate-y-1/2 flex-col justify-center border-2 border-black bg-pink-100 px-5 py-10 lg:h-1/2 lg:w-200">
@@ -53,7 +67,7 @@ const QuizCard: React.FC<IQuizCardProps> = ({ quizId }) => {
           <h2 className="mb-5 text-center text-xl font-bold text-pretty md:mb-10 md:text-2xl">
             {quiz.questionText}
           </h2>
-          <QuizForm quiz={quiz} />
+          <QuizForm quiz={quiz} answers={answersForCurrentQuiz} />
         </>
       )}
       <ul className="mt-auto flex flex-wrap justify-between gap-2 md:justify-around">
@@ -61,15 +75,17 @@ const QuizCard: React.FC<IQuizCardProps> = ({ quizId }) => {
           <ButtonLink link={prevQuiz}>Prev</ButtonLink>
         </li>
 
-        <li className="-order-1 flex w-full grow-1 justify-center self-center md:order-0 md:block md:w-auto md:grow-0">
-          <button
-            form="quiz-form"
-            type="submit"
-            className="text-md w-full cursor-pointer border-2 border-black bg-yellow-400 px-3 py-1 transition-colors duration-300 ease-in hover:bg-yellow-500 md:text-xl"
-          >
-            Submit
-          </button>
-        </li>
+        {/* {!result && (
+          <li className="-order-1 flex w-full grow-1 justify-center self-center md:order-0 md:block md:w-auto md:grow-0">
+            <button
+              form="quiz-form"
+              type="submit"
+              className="text-md w-full cursor-pointer border-2 border-black bg-yellow-400 px-3 py-1 transition-colors duration-300 ease-in hover:bg-yellow-500 md:text-xl"
+            >
+              Submit
+            </button>
+          </li>
+        )} */}
         <li>
           <ButtonLink link={nextQuiz}>Next</ButtonLink>
         </li>
